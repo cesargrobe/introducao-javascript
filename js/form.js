d7554333be7e0d723e8317dcb3565dd7388b0f0f
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2025-12-10 08:26:37 -0300

    Ajuste 3

--- js/form.js
+++ js/form.js
@@ -11,8 +11,7 @@ botaoAdicionar.addEventListener("click", function(event){
 
     var tabela = document.querySelector("#tabela-pacientes");
 
-    //tabela.appendChild(pacienteTr);
-    adicionaPacienteNaTabela(paciente);
+    tabela.appendChild(pacienteTr);
 
     form.reset();
 
@@ -35,26 +34,11 @@ function montaTr(paciente) {
     pacienteTr.classList.add("paciente");
 
     pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-peso"));
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-altura"));
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-gordura"));
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-imc"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
 
 }
-
-function montaTd(dado, classe) {
-    var td = document.createElement("td");
-    td.classList.add(classe);
-    td.textContent = dado;
-
-    return td;
-}
-
-function adicionaPacienteNaTabela(paciente) {
-    var pacienteTr = montaTr(paciente);
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-}
-
